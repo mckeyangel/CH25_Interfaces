972dd8ac36cadc2c2add1ba57f729c4ce11d843e
mckey <miguel.angel.alvarez.mdo@gmail.com>
2023-04-27 08:48:06 -0600

    Se crea clase Cuadrado con su constructor, getters and setters, toString, calcularArea y calcularPerimetro

---
+++ Cuadrado.java
@@ -0,0 +1,39 @@
+
+public class Cuadrado {
+	private String name;
+	private double lado;
+	public Cuadrado(String name, double lado) {
+		this.name = name;
+		this.lado = lado;
+	}//constructor
+	
+	public double calcularArea() {
+		return (Math.pow(getLado(),2));	
+	}//calcularArea
+	public double calcularPerimetro() {
+		return (getLado()*4);
+	}// calcularPerimetro
+
+	public String getName() {
+		return name;
+	}//getName
+
+	public void setName(String name) {
+		this.name = name;
+	}//setName
+
+	public double getLado() {
+		return lado;
+	}//getLado
+
+	public void setLado(double lado) {
+		this.lado = lado;
+	}//setLado
+
+	@Override
+	public String toString() {
+		return "Cuadrado [name=" + name + ", lado=" + lado + "]";
+	}
+	
+
+}//class Cuadrado
